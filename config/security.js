1dd368fb68b678cf7e83629e29101d3fc79c7085
Waleed <wm1989h@gmail.com>
2026-07-02 01:23:52 +0300

    fix: add cartocdn to connectSrc too for map metadata requests

--- config/security.js
+++ config/security.js
@@ -52,7 +52,7 @@ module.exports = {
                 styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com", "https://unpkg.com"],
                 fontSrc: ["'self'", "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
                 imgSrc: ["'self'", "data:", "blob:", "https://unpkg.com", "https://*.tile.openstreetmap.org", "https://tile.openstreetmap.org", "https://*.basemaps.cartocdn.com"],
-                connectSrc: ["'self'", "wss:", "ws:", "https://cdn.jsdelivr.net", "https://unpkg.com", "https://cdnjs.cloudflare.com"],
+                connectSrc: ["'self'", "wss:", "ws:", "https://cdn.jsdelivr.net", "https://unpkg.com", "https://cdnjs.cloudflare.com", "https://*.basemaps.cartocdn.com"],
                 mediaSrc: ["'self'"],
                 frameSrc: ["'self'"],
             },
